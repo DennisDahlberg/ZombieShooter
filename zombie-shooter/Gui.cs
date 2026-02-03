370b7cd16b58326d0fef3c186235b09e561f5519
DennisDahlberg <dennisdalberg02@gmail.com>
2026-02-03 19:14:53 +0100

    Added shake effect to health bar when taking damage

--- zombie-shooter/Gui.cs
+++ zombie-shooter/Gui.cs
@@ -21,6 +21,8 @@ public partial class Gui : CanvasLayer
 	{
 		Color originalHealthColor = Color.FromHtml("#5c1c1c");
 		Color highlightColor = Color.FromHtml("#ff7e7e");
+		float shakeIntensity = 5.0f;
+		Vector2 originalPos = _healthBar.Position;
 		
 		var barStyle = _healthBar.GetThemeStylebox("fill") as StyleBoxFlat;
 		
@@ -31,6 +33,17 @@ public partial class Gui : CanvasLayer
 		tween.Chain().TweenProperty(barStyle, "bg_color", originalHealthColor, 0.3f);
 		
 		tween.TweenProperty(_healthBar, "value", newHealth, 0.4f);
+		
+		var shakeTween = CreateTween();
+		for (int i = 0; i < 6; i++)
+		{
+			var offset = new Vector2(
+				(float)GD.RandRange(-shakeIntensity, shakeIntensity),
+				(float)GD.RandRange(-shakeIntensity, shakeIntensity)
+			);
+			shakeTween.TweenProperty(_healthBar, "position", originalPos + offset, 0.05f);
+		}
+		shakeTween.TweenProperty(_healthBar, "position",  originalPos, 0.05f);
 	}
 
 	public void SetMaxAmmo(int newMaxAmmo)
