7aabf0b85eb16143572a3f5ebf0c129c53f71d89
DennisDahlberg <dennisdalberg02@gmail.com>
2026-02-09 09:56:13 +0100

    Added a try purchase method for perk machine that removes the action label after perk has been bought

--- zombie-shooter/PerkMachine.cs
+++ zombie-shooter/PerkMachine.cs
@@ -9,6 +9,7 @@ public partial class PerkMachine : StaticBody2D
 
 	private Area2D _buyArea;
 	private bool _playerInRange;
+	private bool _isBought;
 
 	public override void _Ready()
 	{
@@ -20,7 +21,7 @@ public partial class PerkMachine : StaticBody2D
 	
 	private void OnBodyEntered(Node2D body)
 	{
-		if (body.IsInGroup("Player"))
+		if (body.IsInGroup("Player") && !_isBought)
 		{
 			GameManager.Instance.UpdateActionLabel("Press F to buy perk [" + Cost + "]");
 			_playerInRange = true;
@@ -33,6 +34,27 @@ public partial class PerkMachine : StaticBody2D
 		{
 			GameManager.Instance.UpdateActionLabel("");
 			_playerInRange = false;
+		}
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (_playerInRange && @event.IsActionPressed("buy") && !_isBought)
+		{
+			TryPurchase();
+		}
+	}
+
+	private void TryPurchase()
+	{
+		if (GameManager.Instance.SpendMoney(Cost))
+		{
+			GameManager.Instance.UpdateActionLabel("");
+			_isBought = true;
+		}
+		else
+		{
+			GD.Print("Unable to buy perk!");
 		}
 	}
 }
