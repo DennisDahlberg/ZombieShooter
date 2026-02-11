78c4876f14503e0788477ea1741fc72bbb82ce4f
DennisDahlberg <dennisdalberg02@gmail.com>
2026-02-11 11:16:08 +0100

    Added spinning gun effect for mysterbox when opened

--- zombie-shooter/Mysterybox.cs
+++ zombie-shooter/Mysterybox.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 using Godot.Collections;
 using ZombieShooter.Gun;
 
@@ -13,9 +14,10 @@ public partial class Mysterybox : StaticBody2D
 	private AnimatedSprite2D _sprite;
 	private Marker2D _spawnPoint;
 	private Timer _spinTimer;
+	private Timer _spinCooldownTimer;
 	private Sprite2D _gunSprite;
 	private Area2D _buyArea;
-	
+
 	private bool _isSpinning = false;
 	private bool _isPlayerInRange = false;
 
@@ -24,9 +26,11 @@ public partial class Mysterybox : StaticBody2D
 		_sprite = GetNode<AnimatedSprite2D>("Sprite");
 		_spawnPoint = GetNode<Marker2D>("WeaponSpawnPoint");
 		_spinTimer = GetNode<Timer>("SpinTimer");
+		_spinCooldownTimer = GetNode<Timer>("SpinCooldown");
 		_buyArea = GetNode<Area2D>("BuyArea");
 
 		_gunSprite = new Sprite2D();
+		_gunSprite.ZIndex = 1000;
 		_spawnPoint.AddChild(_gunSprite);
 		_gunSprite.Hide();
 		
@@ -34,17 +38,46 @@ public partial class Mysterybox : StaticBody2D
 		
 		_buyArea.BodyEntered += OnBodyEntered;
 		_buyArea.BodyExited += OnBodyExited;
+		_spinTimer.Timeout +=  OnTimerTimeout;
 	}
 
 	public override void _Process(double delta)
 	{
-		if (_isSpinning)
+		if (!_isSpinning || !_spinCooldownTimer.IsStopped())
+			return;
+		
+		_spinCooldownTimer.Start(0.2f);
+		int randomIndex = (int)(GD.Randi() % WeaponPool.Count);
+		_gunSprite.Texture = WeaponPool[randomIndex].Icon;
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (_isPlayerInRange && !_isSpinning && @event.IsActionPressed("buy"))
 		{
-			int randomIndex = (int)(GD.Randi() % WeaponPool.Count);
-			_gunSprite.Texture = WeaponPool[randomIndex].Icon;
+			StartSpin();
 		}
 	}
-	
+
+	private void StartSpin()
+	{
+		_isSpinning = true;
+		_sprite.Play("open");
+		_gunSprite.Show();
+		_spinTimer.Start(SpinDuration);
+	}
+
+	public void OnTimerTimeout()
+	{
+		GD.Print("Spin timer stopped");
+		StopSpin();
+	}
+
+	private void StopSpin()
+	{
+		_isSpinning = false;
+	}
+
 	private void OnBodyEntered(Node body)
 	{
 		if (body is Player)
